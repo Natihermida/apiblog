c928647704e67d0ffc68a5e4a2f8f7f96ce16724
NatiHermida <hermidaltamarnati@gmail.com>
2025-05-02 16:59:19 +0200

    bd_ok

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT 
-  
+  port: process.env.DB_PORT || 3306 // Puerto por defecto de MySQL
 });
 
 db.connect((err) => {
